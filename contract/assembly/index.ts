6aefaf3f324c17e237b1fb95fa97cf4f722ae601
Crystal <vertfromage@gmail.com>
2022-04-12 14:39:36 -0300

    added index.ts model.ts as in my score tutorial

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,34 +1,68 @@
-/*
- * This is an example of an AssemblyScript smart contract with two simple,
- * symmetric functions:
- *
- * 1. setGreeting: accepts a greeting, such as "howdy", and records it for the
- *    user (account_id) who sent the request
- * 2. getGreeting: accepts an account_id and returns the greeting saved for it,
- *    defaulting to "Hello"
- *
- * Learn more about writing NEAR smart contracts with AssemblyScript:
- * https://docs.near.org/docs/develop/contracts/as/intro
- *
- */
-
 import { Context, logging, storage } from 'near-sdk-as'
+import { HighScore, highScoreList } from './model';
 
-const DEFAULT_MESSAGE = 'Hello'
-
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+export function addToScore(value: i32): void {
+  const key = Context.sender+"score";
+  const newScore = storage.getPrimitive<i32>(key, 0) + value;
+  storage.set<i32>(key, newScore);
+}
+export function subFromScore(value: i32): void {
+  const key = Context.sender+"score";
+  const newScore = storage.getPrimitive<i32>(key, 0) - value;
+  storage.set<i32>(key, newScore);
+}
+export function getScore(accountId: string): i32 {
+  return storage.getPrimitive<i32>(accountId+"score", 0);
+}
+export function resetScore(): void {
+  const key = Context.sender+"score";
+  storage.set<i32>(key, 0);
 }
 
-export function setGreeting(message: string): void {
-  const accountId = Context.sender
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(`Saving greeting "${message}" for account "${accountId}"`)
-  storage.set(accountId, message)
+// High Score Code
+
+const HIGHSCORE_LIST_SIZE = 10;
+
+/**
+ * 
+ * @param score 
+ * if list length less than 10 add score
+ * then find smallest score and compare to newScore if newScore is bigger swap
+ */
+export function addHighScore(score: u32): void {
+  const newScore = new HighScore(score);
+  // until list up to size add score
+  if(highScoreList.length < HIGHSCORE_LIST_SIZE){
+    highScoreList.push(newScore);
+    return;
+  }
+  // find smallest score in array
+  let smallestScoreIndex = 0;
+  for(let i=1;i<highScoreList.length;i++){
+    if(highScoreList[i].score < highScoreList[smallestScoreIndex].score){
+      smallestScoreIndex = i;
+    }
+  }
+  //compare smallest to newScore if newScore is bigger swap
+  if(highScoreList[smallestScoreIndex].score<newScore.score){
+    highScoreList.replace(smallestScoreIndex,newScore);
+  }
+}
+
+/*
+* Returns an array of highscores, unsorted.
+*/
+export function getHighScoreList(): HighScore[]{
+  const result = new Array<HighScore>(highScoreList.length);
+  for(let i = 0; i < highScoreList.length; i++) {
+    result[i] = highScoreList[i];
+  }
+  return result;
+}
+
+
+export function resetHighScoreList(): void{
+ while(highScoreList.length>0){
+   highScoreList.pop();
+ } 
 }
---
+++ contract/assembly/model.ts
@@ -0,0 +1,21 @@
+import { context, PersistentVector } from "near-sdk-as";
+
+/** 
+ * Exporting a new class HighScore so it can be used outside of this file.
+ */
+ @nearBindgen
+ export class HighScore {
+   player: string;
+   constructor(public score: u32) {
+     this.player = context.sender;
+   }
+ }
+
+ /**
+ * collections.vector is a persistent collection. Any changes to it will
+ * be automatically saved in the storage.
+ * The parameter to the constructor needs to be unique across a single contract.
+ * It will be used as a prefix to all keys required to store data in the storage.
+ * // Unsorted array of 10 biggest scores
+ */
+export const highScoreList = new PersistentVector<HighScore>("s");
